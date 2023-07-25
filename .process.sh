d773efa5affc53625ba6eb7130a7ecb5f81e2401
Tianon Gravi <admwiggin@gmail.com>
2023-07-25 15:34:56 -0700

    Build just a subset (and currently map that to just notary)

--- .process.sh
+++ .process.sh
@@ -6,8 +6,10 @@ dir="$(dirname "$BASH_SOURCE")"
 unset "${!BASHBREW_@}" # just to be sure
 export BASHBREW_LIBRARY="$dir/.doi/library"
 
+subset="$(< "$dir/subset.txt")"
+
 set -x
 
-time "$dir/.scripts/sources.sh" > "$dir/sources.json"
+time "$dir/.scripts/sources.sh" $subset > "$dir/sources.json"
 
 time "$dir/.scripts/builds.sh" "$dir/sources.json" > "$dir/builds.json"
